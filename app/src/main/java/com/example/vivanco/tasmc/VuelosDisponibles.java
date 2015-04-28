1e3dfc1cba6de465b68e8f38fe6e64af20c0eee6
erickvica <erickvivanco01@hotmail.com>
2015-04-28 11:31:19 -0500

    app_bar for vuelos

--- app/src/main/java/com/example/vivanco/tasmc/VuelosDisponibles.java
+++ app/src/main/java/com/example/vivanco/tasmc/VuelosDisponibles.java
@@ -1,7 +1,8 @@
 package com.example.vivanco.tasmc;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,12 @@ public class VuelosDisponibles extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vuelos_disponibles);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar_vuelo);
+        setSupportActionBar(toolbar);
+
+        //Habilita el boton para ir a la actividad principal en el Toolbar
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
